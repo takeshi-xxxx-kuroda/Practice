1c893fab0b2c499418632d551a8ac4bda77f39d1
Takeshi Kuroda <mengshiheitian@gmail.com>
2020-06-23 22:44:41 +0900

    update

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	#msg = 'Good'
+	return render_template('index.html',msg='Good morning')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=80)
